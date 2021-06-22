b87547040061aa0579d5820638bddaeb1f6ac5d6
samratpradhan <samrattpradhann@gmail.com>
2021-06-22 16:26:53 +0545

    initial commit

---
+++ src/App1.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data1';
+function App1() {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = React.useState(0);
+
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 5000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [index]);
+
+  return (
+    <section className="section">
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
+
+          let position = 'nextSlide';
+          if (personIndex === index) {
+            position = 'activeSlide';
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = 'lastSlide';
+          }
+
+          return (
+            <article className={position} key={id}>
+              
+                <img src={image} alt={name} className="person-img" />
+              <h2>{name}</h2>
+              <h3 className="title">{title}</h3>
+              <h3 className="text">{quote}</h3>
+              <FaQuoteRight className="icon" />
+              
+            </article>
+          );
+        })}
+        <button className="prev" onClick={() => setIndex(index - 1)}>
+          <FiChevronLeft />
+        </button>
+        <button className="next" onClick={() => setIndex(index + 1)}>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+    
+  );
+}
+
+export default App1;
---
+++ src/Primary.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import App from './App'
+import App1 from './App1'
+const Primary=()=>{
+return(
+<div>
+<App />
+<App1 />
+</div>
+);
+}
+export default Primary;
